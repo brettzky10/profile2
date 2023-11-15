16fb8eabf3ddbd1a9ba2a84e8d4c07d9db03c047
brettzky10 <brett.linseman@gmail.com>
2023-11-15 15:15:00 -0500

    fixed wording

--- lib/data.ts
+++ lib/data.ts
@@ -78,9 +78,9 @@ export const projectsData = [
     link: "https://outdoor-store-hjlx2igkf-brettzky10.vercel.app/"
   },
   {
-    title: "Arcitectural Website",
+    title: "Architectural Website",
     description:
-      "A website for an arcitectural brand that showcases parallax and other packages.",
+      "A website for an architectural brand that showcases a modern website layout.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind"],
     imageUrl: archcompanyImg,
     link: "https://architecture-company-3heg3jfvk-brettzky10.vercel.app/"
